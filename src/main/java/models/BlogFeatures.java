70daaad27a071ece0774e8ee37c0646ae62cb960
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-25 10:44:03 -0700

    add details to 1:many and many:many classes

--- src/main/java/models/BlogFeatures.java
+++ src/main/java/models/BlogFeatures.java
@@ -4,4 +4,64 @@ package models;
  * Created by Guest on 8/25/17.
  */
 public class BlogFeatures {
+    private String writtenBy;
+    private int rating;
+    private int storeId;
+    private int id;
+
+    public BlogFeatures(String writtenBy, int rating, int storeId){
+        this.writtenBy = writtenBy;
+        this.rating = rating;
+        this.storeId = storeId;
+    }
+
+    //Getters
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public int getStoreId() {
+        return storeId;
+    }
+    public int getId() {
+        return id;
+    }
+
+    //Setters
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Equals and HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogFeatures that = (BlogFeatures) o;
+
+        if (rating != that.rating) return false;
+        if (storeId != that.storeId) return false;
+        return writtenBy.equals(that.writtenBy);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = writtenBy.hashCode();
+        result = 31 * result + rating;
+        result = 31 * result + storeId;
+        return result;
+    }
 }
--- src/main/java/models/StoreType.java
+++ src/main/java/models/StoreType.java
@@ -4,4 +4,47 @@ package models;
  * Created by Guest on 8/25/17.
  */
 public class StoreType {
+    private String name;
+    private int id;
+
+    public StoreType(String name){
+        this.name = name;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    //Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Equals and HashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StoreType storeType = (StoreType) o;
+
+        if (id != storeType.id) return false;
+        return name.equals(storeType.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
 }
